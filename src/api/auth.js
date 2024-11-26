acad4aba153f14cd5d636666d4b594d6c84b8971
Santiagoservera <santiagoservera21@gmail.com>
2024-11-26 13:33:14 -0300

    funcion cambiada

--- src/api/auth.js
+++ src/api/auth.js
@@ -4,4 +4,6 @@ export const registerRequest = (user) => axios.post(`/register`, user);
 
 export const loginRequest = (user) => axios.post(`/login`, user);
 
-export const verifyTokenRequest = () => axios.get(`/verify`);
+export const verifyTokenRequest = async () => {
+  return axios.get("/auth/verifyToken", { withCredentials: true });
+};
